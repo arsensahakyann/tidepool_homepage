adae83164159d28fd7b3b7a4c5a1cd7a58dea7af
arsensahakyann <arsensahakyan@tidepoollabs.com>
2024-03-11 09:44:38 -0700

    the project is finely finished

--- script.js
+++ script.js
@@ -7,5 +7,30 @@ window.addEventListener('scroll', function(){
     }else{
         header.classList.remove("fixed");
     }
-
 })
+
+
+const carouselItems = document.querySelectorAll('.review');
+let currentIndex = 0;
+
+function showSlide(index) {
+  carouselItems.forEach(item => {
+    item.style.display = 'none';
+  });
+  carouselItems[index].style.display = 'block';
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % carouselItems.length;
+  showSlide(currentIndex);
+}
+
+function previousSlide() {
+  currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+  showSlide(currentIndex);
+}
+
+showSlide(currentIndex);
+
+document.getElementById('nextBtn').addEventListener('click', nextSlide);
+document.getElementById('prevBtn').addEventListener('click', previousSlide);
